f8fd96df72adaa6b3d5288113e5bf1a9544cfb29
Nishant Bisht <116999283+a3s0h@users.noreply.github.com>
2024-02-05 23:28:46 +0530

    Update App.jsx

--- e-com/src/App.jsx
+++ e-com/src/App.jsx
@@ -1,6 +1,6 @@
 // App.jsx
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Products from './components/Products'
 import AddProduct from './components/AddProducts'
@@ -9,22 +9,15 @@ import HomePage from './components/Homepage'
 function App() {
   const [user, setUser] = useState(null);
 
-  const handleLogin = () => {
-    // Perform login logic and set the user state
-    setUser({ name: "John Doe" });
-  };
 
-  const handleLogout = () => {
-    // Perform logout logic and reset the user state
-    setUser(null);
-  };
 
   return (
     <Router>
-      <Route path='/' element={ <HomePage/> }/>
-      <Route path='/get' element={ <Products/> } />
-      <Route path='/add' element={ <AddProduct/> } />
-                
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/get' element={<Products />} />
+        <Route path='/add' element={<AddProduct />} />
+      </Routes>
     </Router>
   );
 }
