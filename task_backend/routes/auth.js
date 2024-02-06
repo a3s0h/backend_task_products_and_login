5f3451fc537764eec5c776dec56e40d60c78d83d
Nishant Bisht <116999283+a3s0h@users.noreply.github.com>
2024-02-06 09:34:00 +0530

    fixing paths in router

--- task_backend/routes/auth.js
+++ task_backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, logoutUser, authChecker } = require("../controllers/AuthController");
+const { registerUser, loginUser, logoutUser, authChecker } = require("../controllers/auth.js");
 
 router.post("/register",registerUser );
 
